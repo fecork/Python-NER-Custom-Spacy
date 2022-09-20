b3fc1125c2b611ccd0a1e87b077341e8ee423934
Ferney Córdoba Canchala <fecork88@gmail.com>
2022-09-20 10:37:29 -0500

    🚧 build(tsg): datos etiquetados

--- sort_data_to_tag.py
+++ sort_data_to_tag.py
@@ -1,6 +1,8 @@
+from dataclasses import replace
 import os
+import re
 
-path = "/Proyectos/Spacy-NER/data"
+path = "/Proyectos/Spacy-NER/Python-NER-Custom-Spacy/data/"
 
 os.chdir(path)
 
@@ -11,10 +13,27 @@ def read_text_file(file_path):
         list_text.append(f.read())
 
     with open("/Proyectos/Spacy-NER/train.txt", "a") as f:
-        f.write(str(list_text[0]))
+        texto = str(list_text[0])
+        texto = format_text(texto)
+        f.write(texto)
         f.write("\n")
         f.write("---")
         f.write("\n")
+
+
+def format_text(text: str) -> str:
+    """Preprocesses the data text, clean it.
+
+    Args:
+        text: String Raw data.
+    Returns:
+        Preprocessed data text, without stranger character.
+    """
+
+    text = text.replace(str("\\n"), "\n")
+    text = text.replace(str("/"), " ")
+    text = re.sub(r"[^a-zA-Z0-9\s\n.,;]", "", text)
+    return text
 
 
 for file in os.listdir():
